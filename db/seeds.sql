dda63b5781a69c8f7c5e0f99304c21163860e33f
Marcus Johnson <597392@park.edu>
2018-06-08 23:16:14 -0500

    assets

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon CheeseBurger',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pattie Melt',false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beff Egg Burger',false);
---
+++ server.js
@@ -0,0 +1,25 @@
+// Dependencies 
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require('method-override');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use('/', routes);
+
+app.listen(PORT);
